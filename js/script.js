b78dbed2cb379493e9a408886af6d4dbf3e8a215
ALGORITHIM574 <githinjidci0437@gmail.com>
2026-06-17 06:40:38 +0300

    upgrade to the application

---
+++ js/script.js
@@ -0,0 +1,55 @@
+// CONTRIBUTION MODAL
+const BTN = document.querySelector("#btn");
+const modal = document.querySelector("#modal");
+
+if (BTN && modal) {
+  BTN.addEventListener("click", function () {
+    console.log("contribution added"); //for debugging
+    modal.style.display = "block";
+  });
+}
+// SEARCH MEMBERS
+const search = document.querySelector("#search-member");
+
+if (search) {
+  search.addEventListener("keyup", function (e) {
+    const term = e.target.value.toLowerCase();
+
+    const tbody = document.querySelector("tbody");
+
+    if (!tbody) return;
+
+    const rows = tbody.getElementsByTagName("tr");
+
+    Array.from(rows).forEach(function (row) {
+      const name = row.children[1].textContent.toLowerCase();
+
+      if (name.includes(term)) {
+        row.style.display = "";
+      } else {
+        row.style.display = "none";
+      }
+    });
+  });
+}
+// CLOSE CONTRIBUTION MODAL
+const saveBtn = document.querySelector("#save");
+
+if (saveBtn && modal) {
+  saveBtn.addEventListener("click", function () {
+    console.log("saved"); //for debugging
+    modal.style.display = "none";
+  });
+}
+
+// MEMBER MODAL
+
+const memberBtn = document.querySelector("#btn-2");
+const modal2 = document.querySelector("#modal-2");
+
+if (memberBtn && modal2) {
+  memberBtn.addEventListener("click", function () {
+    console.log("member added"); //for debugging
+    modal2.style.display = "block";
+  });
+}
